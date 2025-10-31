8c9adefb9128492d954ec20d618e7cb7f8476fe9
Paweł Wydrych <pawel.wydrych2002@gmail.com>
2025-10-31 10:39:18 +0100

    kedro

---
+++ src/sprint2_kedro/pipelines/asi/pipeline.py
@@ -0,0 +1,20 @@
+from kedro.pipeline import Pipeline, node
+from .nodes import load_raw, basic_clean, split_data, train_baseline, evaluate
+
+
+def create_pipeline(**kwargs):
+    return Pipeline(
+        [
+            node(load_raw, "raw_data", "loaded_data"),
+            node(basic_clean, "loaded_data", "clean_data"),
+            node(
+                split_data,
+                ["clean_data", "params:split.test_size", "params:split.random_state"],
+                ["X_train", "X_test", "y_train", "y_test"],
+            ),
+            node(
+                train_baseline, ["X_train", "y_train", "params:model"], "model_baseline"
+            ),
+            node(evaluate, ["model_baseline", "X_test", "y_test"], "metrics_baseline"),
+        ]
+    )
---
+++ tests/pipelines/asi/test_split.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from sprint2_kedro.pipelines.asi.nodes import split_data
+
+
+def test_split_data_simple():
+    df = pd.DataFrame(
+        {
+            "class": [0, 1, 0, 1, 0],
+            "feat1": [10, 20, 30, 40, 50],
+            "feat2": ["a", "b", "c", "d", "e"],
+        }
+    )
+
+    test_size = 0.4
+    random_state = 42
+
+    X_train, X_test, y_train, y_test = split_data(df, test_size, random_state)
+
+    assert len(X_train) == 3
+    assert len(X_test) == 2
+    assert len(y_train) == 3
+    assert len(y_test) == 2
+
+    # brak target leakate
+    assert "class" not in X_train.columns
+    assert "class" not in X_test.columns
